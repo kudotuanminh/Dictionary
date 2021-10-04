4a60b5c658e19a59641f985f04c67f49d08df8be
20020198uet <hdv250202@gmail.com>
2021-10-04 17:23:57 +0700

    ADD Export.txt

--- src/main/java/com/ntm/dictionary/Main.java
+++ src/main/java/com/ntm/dictionary/Main.java
@@ -9,7 +9,7 @@ public class Main {
         //Export to File dictionaries.txt
         // source:https://stackoverflow.com/questions/6548157/how-to-write-an-arraylist-of-strings-into-a-text-file
         try {
-            FileWriter fw = new FileWriter("C:\\Users\\Vo Dinh Huy\\OneDrive\\Documents\\GitHub\\Dictionary\\resources\\dictionaries.txt");
+            FileWriter fw = new FileWriter("C:\\Users\\Vo Dinh Huy\\OneDrive\\Documents\\GitHub\\Dictionary\\resources\\export.txt");
             for (int i = 0; i < Dictionary.words.size(); i++) {
                 fw.write(Dictionary.words.get(i).getWordTarget() + "\n");
                 fw.write(Dictionary.words.get(i).getWordExplain() + "\n");
